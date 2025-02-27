05e66ad68e508aa4311c684afd86e1d490621346
marcindz2@prokonto.pl <25087@KI.LOCAL>
2025-02-27 11:10:34 +0100

    dodanie 1 zadania

---
+++ lab1/skrypt2-25087.py
@@ -0,0 +1,15 @@
+# def palindrome(word: str) -> str:
+#     """
+#     Funkcja ma za zadanie odwrócić sentencję podaną przez użytkownika
+#     :param word: wejściowy łańcuch znaków
+#     :return: palindrom wejściowego łańcucha znaków
+#     """
+#     return word[::-1]
+
+
+# if __name__ == '__main__':
+#     sentencja = input('Podaj ciąg znaków: ')
+#     print(f"Palindrom ciągu wejściowego to: {palindrome(sentencja)}")
+
+# Napisz program, który sprawdza czy wczytany łańcuch znakowy jest liczbą lub nie. Muszą być wczytane co najmniej dwa znaki.
+# Hint: skorzystaj z funkcji all().
